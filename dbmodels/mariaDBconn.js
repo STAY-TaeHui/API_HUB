551353ea16725fdb91cb2717855b9b261970b41e
SeoTaeHui <masta95@naver.com>
2020-08-20 14:35:04 +0900

    DB연결 성공/config-gitignore

--- dbmodels/mariaDBconn.js
+++ dbmodels/mariaDBconn.js
@@ -1,11 +1,12 @@
 var mariadb = require('mariadb');
 const pool = mariadb.createPool(
   {
-    database:process.env.TESTDB_DATABASE,
-    host: process.env.TESTDB_HOST,
-    port: process.env.TESTDB_PORT,
-    user: process.env.TESTDB_USER,
-    password: process.env.TESTDB_PASSWORD,
+    database:process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    timezone: process.env.DB_TIMEZONE,
     connectionLimit: 1
   }
 );
@@ -15,8 +16,6 @@ const getUserList = async (req,res) =>{
   try {
     conn = await pool.getConnection();
     console.log("--------------------------------CONNECTION SUCCEES----------------------------------")
-    //  conn.query('mysql'); // 사용할 DB 명시
-    rows = await conn.query('SELECT * FROM db'); // 쿼리 실행
   }
   catch (err) { 
     console.log("CONNECTION FAILE");
